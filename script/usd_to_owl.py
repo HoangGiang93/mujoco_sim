db3382eb9f915c1456b60bbb9aa77e57ac591017
Giang Nguyen <hoanggia@uni-bremen.de>
2023-03-02 21:27:13 +0100

    Update usd_to_owl

--- script/usd_to_owl.py
+++ script/usd_to_owl.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
-from pxr import Usd, UsdGeom, Sdf, Gf, UsdPhysics
+from pxr import Usd
+from owlready2 import onto_path, get_ontology
 
-
+types = set()
+prim_types = dict()
 def usd_to_owl(file_path: str) -> None:
     stage = Usd.Stage.Open(file_path)
     for prim in stage.Traverse():
-        for attr in prim.GetAttributes():
-            if isinstance(attr.Get(), str):
-                print(attr, dir(attr))
+        for prop in prim.GetPropertyNames():
+            if 'semanticType' in prop and prim.GetAttribute(prop).Get() == 'class':
+                prop = prop.replace('Type', 'Data')
+                type = prim.GetAttribute(prop).Get()
+                types.add(type)
+                prim_types[prim.GetName()] = type
+                break
+
+    onto_path.append("/home/giang/Workspace/mujoco_ws")
+    onto = get_ontology("http://www.lesfleursdunormal.fr/static/_downloads/pizza_onto.owl")
+    onto.load()
+    onto.save()
     return None
 
 
